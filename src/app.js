776c8e2b95652f405e490fdd55514ddb8b7a5812
Aaddityaaa <adityau5090@gmail.com>
2026-06-27 16:44:13 +0530

    make routes to ftch all users

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import notificationRoutes from "./routes/notification.routes.js";
 import adminRoutes from "./routes/admin.routes.js";
 import authRoutes from "./routes/auth.routes.js"
+import userRoutes from "./routes/user.routes.js"
+import { protect } from "./middleware/auth.middleware.js";
 
 
 
@@ -18,12 +20,14 @@ App.use(express.json());
 App.use("/api/notifications", notificationRoutes);
 App.use("/api/admin", adminRoutes);
 App.use("/api/auth", authRoutes);
+App.use("/users", adminRoutes, userRoutes)
 App.get("/", (req, res) => {
   res.json({
     success: true,
     message: "ZenStreak Backend Running 🚀",
   });
 });
+
 
 
 export { App };
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { sendBroadcastNotification, sendUserNotification } from "../controllers/admin.controller.js";
 import { verifyAdmin } from "../middleware/admin.middleware.js";
+import { protect } from "../middleware/auth.middleware.js";
 
 
 const router = Router();
 
-router.post("/send-notification",verifyAdmin,sendBroadcastNotification);
-router.post("/send-user-notification",verifyAdmin, sendUserNotification);
+router.post("/sendBroadcastNotification",protect,sendBroadcastNotification);
+router.post("/sendUserNotification",protect, sendUserNotification);
 
 export default router;
